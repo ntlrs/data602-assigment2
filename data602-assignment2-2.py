07c992c3e8d9755bb193ee6597bacb27f547f35e
Natalie Mollaghan <natalie.mollaghan29@spsmail.cuny.edu>
2018-05-24 18:29:02 -0400

    no message

--- data602-assignment2-2.py
+++ data602-assignment2-2.py
@@ -16,15 +16,24 @@ from datetime import datetime, timedelta
 from pymongo import MongoClient
 from time import strftime, gmtime
 from statistics import stdev
+import urllib
+
+
+
 
 def main():    
    # Initial portfolio size in cash
     funds = 100000000.0
     pair = ["BCH-USD","BTC-USD","ETH-USD","LTC-USD"] # assume usd base
+    
+    db = MongoClient('localhost', 27017)
+    db_blotter = db["blotter"]["blotter"]
 
+    
    # Initialize data structures
-    df_blotter = initialize_blotter()
-    df_pl = initialize_pl(pair)
+    cols = ['Ticker','Qty','Price','Date','Cost','Cash']
+    blotter = pd.DataFrame(columns= cols)
+    pl = initialize_pl(pair)
 
    # Design a menu a system
     menu = ('Buy', 'Sell', 'Show Blotter', 'Show PL', 'Quit')
@@ -36,13 +45,14 @@ def main():
                 ask, bid = get_price(pair)               
                 price = float(ask)
                 total_cost = float(qty * price)
+                total_costs = str(total_cost)
                 show_stats(pair)
-                print("Buying "+ str(qty) + " share of " + pair + " for " + str(total_cost))
-                blotter = trade(pair, qty, price, total_cost)
-                df_blotter.append(blotter)
-                return df_blotter   
+                print("Buying "+ str(qty) + " share of " + pair + " for " + total_costs)
+                blotter = trade(pair, qty, price, total_cost, funds, db_blotter)
+                main()
+                return blotter
             else:
-                pass
+                sys.exit()
            
         elif choice == 2:
             pair, qty = select_crypto()
@@ -51,24 +61,25 @@ def main():
                 ask, bid = get_price(pair)
                 price = float(bid)
                 total_cost = float(qty * price)
+                total_costs = str(total_cost)
                 show_stats(pair)
-                print("Selling " + str(qty) + " share of " + pair + " for " + str(total_cost))
-                blotter = trade(pair, qty, price, total_cost)
-                df_blotter.append(blotter)
-                return df_blotter
+                print("Selling " + str(qty) + " share of " + pair + " for " + total_costs)
+                blotter = trade(pair, qty, price, total_cost, funds, db_blotter)
+                print(blotter)
+                main() 
+                return blotter
             else:
-                pass
+                main()
             
         elif choice == 3:
-            view_blotter(df_blotter)
+            view_blotter(blotter)
         elif choice == 4:
-            view_pl(df_pl)
+            update_pl()
         elif choice == 5:
-            quit()
+            break
         else:
           print("Not An Option ")
           return
-            
 
 def display_menu(menu,exit_option=-1):
     for m in menu:
@@ -79,17 +90,80 @@ def display_menu(menu,exit_option=-1):
         quit()
     return choice
 
-def trade(pair, qty, price, total_cost):
+def trade(pair, qty, price, total_cost, funds, db_blotter):
+    db = MongoClient('localhost', 27017)
+    db_blotter = db["blotter"]["blotter"]
     blotter = []
-    date = strftime("%Y-%m-%d %H:%M:%S", gmtime()) 
+    date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    fund = funds - total_cost
     confirm = input("Confirm Transaction [Y/N]: ")
     conf = confirm.upper()
-    if conf=="Y" or "y":
-        transaction = [pair, qty, price, date, total_cost]
-        blotter.append(transaction)
+    if conf== "Y":
+        transaction1 = [pair, qty, price, date, total_cost, fund]
+        blotter.append(transaction1)
+        transaction = {"Symbol": pair, "Quantity": qty, "Price":price,"Date":date,"Total Cost":total_cost,'Cash':fund}
+        transaction_id = db_blotter.insert_one(transaction).inserted_id
+        print(blotter)
         print("Tranaction Complete")
-    return blotter
-         
+        return transaction_id 
+    else:
+        main()
+
+
+def calc_vwap(current_qty,current_vwap,qty,price):
+    dollar = current_qty * current_vwap
+    new_dollar = dollar + (qty * price)
+    new_qty = current_qty + qty
+    new_vwap = new_dollar / new_qty
+    return new_vwap
+
+
+def update_pl():
+
+    
+    db = MongoClient('localhost', 27017)
+    db_blotter = db["blotter"]["blotter"]
+
+
+    pl = pd.DataFrame(list(db_blotter.find()), dtype=float)
+
+    
+    pl = pl[["Symbol", "Quantity","Price","Date","Total Cost","Cash"]]
+    pl1 = pl[["Symbol", "Quantity", "Price", "Total Cost", "Cash"]]
+    pl = pl1.groupby(["Symbol"]).agg({"Quantity": "sum", "Price": "mean", "Total Cost":"sum", "Cash":"sum"})
+        
+    qty = pl["Quantity"]
+    tot_cost = pl["Total Cost"]
+    vwap = tot_cost/qty
+    pl["VWAP"] = vwap
+    
+    current_qty = pl['Quantity']
+    current_vwap = pl['VWAP']
+    
+    price = pl[ "Price"]
+    
+    mkt_value = current_qty * current_vwap
+    upl = (price * qty) - mkt_value
+    pl["UPL"] = upl
+    
+    
+    rpl =  (price - vwap) * qty
+    pl["RPL"] = rpl
+    
+    total_pl = upl + rpl
+    pl['Total PL'] = total_pl
+    
+    tot_qty = pl['Quantity'].sum()
+    
+    allocation = tot_qty/pl["Quantity"]
+    pl["Allocated By Share"] = allocation
+    
+    pl.drop(['Price', 'Total Cost', 'Cash'], axis = 1)
+    
+    
+    print(pl)
+    
+    
 
 def show_stats(pair):
     print("\n")
@@ -115,60 +189,16 @@ def select_crypto():
     qty =  int(input("How Many Shares Would You Like To Trade?: "))   
     return pair, qty
 
-def calc_vwap(current_qty,current_vwap,qty,price):
-    dollar = current_qty * current_vwap
-    new_dollar = dollar + (qty * price)
-    new_qty = current_qty + qty
-    new_vwap = new_dollar / new_qty
-    return new_vwap
 
-def update_pl(pl,pair,qty,price):
-    if qty > 0: # buy
-        current_qty = pl.at[pair,'Position']
-        current_vwap = pl.at[pair,'VWAP']
-        new_vwap = calc_vwap(current_qty,current_vwap,qty,price) #bid
-        pl.at[pair,'Position'] = current_qty + qty
-        pl.at[pair,'VWAP'] = new_vwap
-        mkt_value = qty * current_vwap
-        upl = price - mkt_value
-        pl.at[pair, 'UPL'] = upl
-        rpl = pl.at[pair, 'RPL']
-        total_pl = upl + rpl
-        pl.at[pair, 'Total PL'] = total_pl
-        
-        
-        
-        # TODO update 'Allocated By Share', 'Allocated By Dollar'
-        #results = pl.append(results)
-        #add a return
-    
-    elif qty < 0: #sell
-        current_qty = pl.at[pair,'Position']
-        current_vwap = pl.at[pair,'VWAP']
-        new_vwap = calc_vwap(current_qty,current_vwap,qty,price) #ask
-        pl.at[pair, 'Position'] = current_qty - qty
-        pl.at[pair,'VWAP'] = new_vwap
-        rpl = (price - current_vwap) * qty
-        pl.at[pair, 'RPL'] = rpl
-        new_upl = (current_qty * current_vwap) - rpl
-        pl.at[pair, 'UPL'] = new_upl
-        total_pl = upl + rpl
-        pl.at[pair, 'Total PL'] = total_pl
-    
-        print("Insert code handling a sale - recalc UPL,RPL & position")
-
-
-def view_blotter(df_blotter):
+def view_blotter(blotter):
+    db = MongoClient('localhost', 27017)
+    db_blotter = db["blotter"]["blotter"]
     print("Trade Blotter")
-    print(df_blotter)
-    print()
+    blotter_df = pd.DataFrame(list(db_blotter.find()))
+    blotter_df=blotter_df[["Symbol","Quantity","Price","Date","Total Cost","Cash"]]
+    print(blotter_df[::-1]) 
+    return blotter
 
-
-def view_pl(df_pl):
-    # TODO Update UPL here
-    print("Profit & Loss")
-    print(df_pl)
-    print()
 
 
 def get_price(pair):
@@ -179,22 +209,15 @@ def get_price(pair):
     return float(ask), float(bid)
 
 
-def initialize_blotter():
-    cols = ['Side','Ticker','Qty','Price','Date','Cost','Cash']
-    df_blotter = pd.DataFrame(columns= cols)
-    df_blotter.drop(['Cash'], axis = 1, inplace = True) 
-    return df_blotter
 
-
-
-def initialize_pl(pair):
-    col_names = ['Pair','Position','VWAP','UPL','RPL', 'Total PL', 'Allocated By Share', 'Allocated By Dollar']
-    pl = pd.DataFrame(columns=col_names)
-    for p in pair:
-        data = pd.DataFrame([[p,0,0,0,0,0,0,0]] ,columns=col_names)
-        pl = pl.append(data, ignore_index=True)
-    pl = pl.set_index('Pair')
-    return pl
+#def initialize_pl(pair):
+    #col_names = ['Pair','Position','VWAP','UPL','RPL', 'Total PL', 'Allocated By Share', 'Allocated By Dollar']
+    #pl = pd.DataFrame(columns=col_names)
+    #for p in pair:
+        #data = pd.DataFrame([[p,0,0,0,0,0,0,0]] ,columns=col_names)
+        #pl = pl.append(data, ignore_index=True)
+    #pl = pl.set_index('Pair')
+    #return pl
 
 def dailymean(dailymax, dailymin):
     mean = (dailymax + dailymin) / 2
@@ -241,3 +264,4 @@ def load(url,printout=False,delay=0,remove_bottom_rows=0,remove_columns=[]):
 
 if __name__ == "__main__":
     main()
+
